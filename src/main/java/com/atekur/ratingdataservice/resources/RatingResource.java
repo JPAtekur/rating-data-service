4b101c894863312503e19379a0c2d400ff49e242
Atekur Rahman <jp7atik@gmail.com>
2020-07-19 12:04:43 +0600

    Create controller, Models and one endpoint

---
+++ src/main/java/com/atekur/ratingdataservice/resources/RatingResource.java
@@ -0,0 +1,17 @@
+package com.atekur.ratingdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atekur.ratingdataservice.models.Rating;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+}
